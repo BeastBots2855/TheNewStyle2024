ca7165db6e93e285b9f02e3e1dbdfc5e1b1a35dc
BeastBotDriverStation <2855BeastBot@gmail.com>
2024-04-04 13:07:13 -0500

    fix method name

--- src/main/java/frc/robot/commands/Vision/FeederLockOn.java
+++ src/main/java/frc/robot/commands/Vision/FeederLockOn.java
@@ -52,7 +52,7 @@ public class FeederLockOn extends Command {
   @Override
   public void execute() {
     m_ThetaController
-        .setSetpoint(PhotonVision.getTagetAngleRobotToTargetPose(targetPose, m_DriveSubsystem.getPose2d()));
+        .setSetpoint(PhotonVision.getTargetAngleRobotToTargetPose(targetPose, m_DriveSubsystem.getPose2d()));
     double thetaOutput = m_ThetaController
         .calculate(m_DriveSubsystem.getPose2d().getRotation().rotateBy(Rotation2d.fromRadians(Math.PI)).getRadians());
     m_DriveSubsystem.drive(
